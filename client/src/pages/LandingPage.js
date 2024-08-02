7d36c1fb55873ee3d704aa1943fae9d5067c2703
Morningstar616 <ashwathjohn2002@gmail.com>
2024-08-02 23:39:18 +0530

    Home Page Responsive added

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import { Box } from '@mui/material';
+import { Box, Grid, Button, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 import homeImage from '../images/images-removebg-preview.png';
 import AboutMe from './AboutMe';
@@ -16,48 +16,41 @@ const sectionStyle = {
   justifyContent: 'center',
 };
 
-const TextSection = styled('div')({
-  flex: 1,
-  padding: '20px',
-  marginLeft: '100px',
+const TextSection = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(2),
   textAlign: 'left',
-  marginTop: '-100px',
-});
+  [theme.breakpoints.up('md')]: {
+    marginLeft: theme.spacing(10),
+    marginTop: theme.spacing(-10),
+  },
+}));
 
-const Heading = styled('h1')({
+const Heading = styled(Typography)(({ theme }) => ({
   fontSize: '1.5rem',
   margin: '0',
-});
+}));
 
-const SubHeading = styled('h2')({
+const SubHeading = styled(Typography)(({ theme }) => ({
   fontSize: '1.5rem',
   margin: '0',
   color: '#fd6f00',
-});
+}));
 
-const Highlight = styled('div')({
+const Highlight = styled(Typography)(({ theme }) => ({
   fontSize: '3rem',
   fontWeight: 'bold',
   lineHeight: '1.2',
-});
+}));
 
-const HighlightText = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-});
-
-const Text = styled('span')({
-});
-
-const BioText = styled('span')({
+const BioText = styled(Typography)(({ theme }) => ({
   display: 'block',
   fontSize: '0.8rem',
   fontWeight: 'normal',
-  marginTop: '20px',
-});
+  marginTop: theme.spacing(2.5),
+}));
 
-const Button = styled('button')({
-  marginTop: '20px',
+const CustomButton = styled(Button)(({ theme }) => ({
+  marginTop: theme.spacing(2.5),
   padding: '10px 20px',
   fontSize: '1rem',
   fontWeight: 'bold',
@@ -70,50 +63,66 @@ const Button = styled('button')({
   '&:hover': {
     backgroundColor: '#e65c00',
   },
-});
+  [theme.breakpoints.down('sm')]: {
+    padding: '8px 16px',
+    fontSize: '0.9rem',
+  },
+}));
 
-const ImageContainer = styled('div')({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  flex: 1,
-  marginTop: '-90px',
-});
 
-const ImageSection = styled('img')({
-  width: '80%', 
+const ImageSection = styled('img')(({ theme }) => ({
+  width: '80%',
   maxWidth: '500px',
   height: 'auto',
   objectFit: 'cover',
-});
+  marginTop: theme.spacing(-10),
+}));
 
 const LandingPage = () => {
   return (
-    <div>
+    <Box>
       <Navbar />
       <Box id="home" style={sectionStyle}>
-        <TextSection>
-          <Heading>Hi I am</Heading>
-          <Highlight>
-            <HighlightText>
-              Ashwath John
-            </HighlightText>
-          </Highlight>
-          <SubHeading>Software Engineer</SubHeading>
-          <BioText>
-            I am a B.Tech in Information Technology student at Cochin University of Science and Technology (CUSAT), with a keen interest in software development and Cloud/DevOps practices. Eager to apply my skills and knowledge in real-world projects.
-          </BioText>
-          <Button>Hire Me</Button>
-        </TextSection>
-        <ImageContainer>
-          <ImageSection src={homeImage} alt="Home Image" />
-        </ImageContainer>
+        <Grid container spacing={3} alignItems="center">
+          <Grid item xs={12} md={6}>
+            <TextSection>
+              <Heading variant="h1">Hi I am</Heading>
+              <Highlight variant="h1">Ashwath John</Highlight>
+              <SubHeading variant="h2">Software Engineer</SubHeading>
+              <BioText variant="body1">
+                I am a B.Tech in Information Technology student at Cochin University of Science and Technology (CUSAT), with a keen interest in software development and Cloud/DevOps practices. Eager to apply my skills and knowledge in real-world projects.
+              </BioText>
+              <CustomButton variant="contained">Hire Me</CustomButton>
+            </TextSection>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <ImageSection src={homeImage} alt="Home Image" />
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
-      <AboutMe />
-      <Services />
-      <Projects />
-      <Contact />
-    </div>
+
+      {/* About Me */}
+      <Box id="aboutMe" style={sectionStyle}>
+        <AboutMe />
+      </Box>
+
+      {/* Services */}
+      <Box id="services" style={sectionStyle}>
+        <Services />
+      </Box>
+
+      {/* Projects */}
+      <Box id="projects" style={sectionStyle}>
+        <Projects />
+      </Box>
+
+      {/* Contact */}
+      <Box id="contact" style={sectionStyle}>
+        <Contact />
+      </Box>
+    </Box>
   );
 };
 
